4f6985b3ae525a72a760401af998216861359ee1
Feliciano07 <fernandoxhajon122@gmail.com>
2020-08-24 22:09:28 -0600

    Se crearon los metodos de lista, obtener uno, crear, actualizar y eliminar secciones

--- src/controllers/seccion.controller.ts
+++ src/controllers/seccion.controller.ts
@@ -1,4 +1,4 @@
-import {Request, Response} from 'express';
+import {Request, Response, json} from 'express';
 import pool  from '../database';
 
 class SeccionController{
@@ -6,6 +6,48 @@ class SeccionController{
     public seccion(req: Request, res: Response){
         res.json({
             text: 'api/seccion'
+        });
+    }
+
+    public async list(req: Request, res: Response): Promise<void>{
+        const secciones = await pool.query('SELECT * FROM seccion');
+        res.json(secciones);
+    }
+
+    public async getOne(req: Request, res: Response): Promise<any>{
+        const {id} = req.params;
+        const secciones = await pool.query('SELECT * FROM seccion WHERE seccion = ?',[id]);
+        if(secciones.length > 0){
+            return res.json(secciones[0]);
+        }
+        res.status(404).json({
+            text: "the game doesn't exits "
+        });
+    }
+
+    public async create(req: Request, res: Response): Promise<void> {
+        const result = await pool.query('INSERT INTO seccion set ?', [req.body]);
+        res.json({
+            text: 'seccion save'
+        });
+
+    }
+
+    public async update(req: Request, res: Response): Promise<void>{
+        const {id} = req.params;
+        const oldseccion = req.body;
+        await pool.query('UPDATE seccion set ? WHERE seccion = ? ', [oldseccion, id]);
+        res.json({
+            text: 'the seccion was update'
+        });
+
+    }
+
+    public async delete(req: Request, res: Response): Promise<void>{
+        const {id} = req.params ; 
+        await pool.query('DELETE FROM seccion WHERE seccion = ?', [id]);
+        res.json({
+            text: ' the seccion was delete'
         });
     }
 }
---
+++ src/models/seccion.interface.ts
@@ -0,0 +1,8 @@
+export interface Seccion{
+    seccion ?: number;
+    nombre ?: string;
+    estado ?: number;
+    grado ?: number;
+    personal ?: number;
+    ciclo ?: number
+}
