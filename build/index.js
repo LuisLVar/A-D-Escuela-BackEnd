6b1533fa4845dcbed53a4e42bc2c5394729ccd76
LuisLVar <3005139410101@ingenieria.usac.edu.gt>
2020-08-24 22:00:13 -0600

    Cambio de puerto

--- build/index.js
+++ build/index.js
@@ -17,7 +17,7 @@ var Server = /** @class */ (function () {
         this.routes();
     }
     Server.prototype.config = function () {
-        this.app.set('port', process.env.PORT || 3000); //el process es para que si ya exite un puerto definido se toma ese o sino agarra el 3000
+        this.app.set('port', process.env.PORT || 3005); //el process es para que si ya exite un puerto definido se toma ese o sino agarra el 3000
         //ese set es de app, es como si se le hubiera declarado una variable a app
         this.app.use(morgan_1.default('dev')); //el dev es para ver lo que estan pidiendo los clientes
         this.app.use(cors_1.default()); //pedir los datos al servidor 
