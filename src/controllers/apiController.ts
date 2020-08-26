5c6a87e2f23d6b7a92a4756ee409ae445ff89bc4
gerchay <2995058550101@ingenieria.usac.edu.gt>
2020-08-26 10:16:32 -0600

    Sintaxis funciones

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -6,14 +6,14 @@ class ApiController {
 //Materia
 
 public async insertarMateria(req: Request, res: Response) {
-  const materia = await pool.query('insertar_materia(?, ?)', [req.body.nombre, req.body.contenido]);        
+  const materia = await pool.query('SELECT insertar_materia(?, ?)', [req.body.nombre, req.body.contenido]);        
       res.json(materia);
   
 }
 
 public async obtenerMateria(req: Request, res: Response) {
   const {id} = req.params;
-  const materia = await pool.query('obtener_materia(?)',[id]);
+  const materia = await pool.query('SELECT obtener_materia(?)',[id]);
   res.json(materia);
   
 }
@@ -25,14 +25,14 @@ public async obtenerMaterias(req: Request, res: Response) {
   
 public async actualizarMateria(req: Request, res: Response) {
   let emp = req.body;
-  const materia = await pool.query('actualizar_materia(?, ?, ?)',[emp.id, emp.nombre, emp.contenido]);
+  const materia = await pool.query('SELECT actualizar_materia(?, ?, ?)',[emp.id, emp.nombre, emp.contenido]);
   res.json(materia);
 }
   
   
   public async eliminarMateria(req: Request, res: Response) {
   let emp = req.body;
-  const materia = await pool.query('eliminar_materia(?)',[emp.id]);
+  const materia = await pool.query('SELECT eliminar_materia(?)',[emp.id]);
   res.json(materia);
 }
 
