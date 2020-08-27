d588088224382e81b78309819f605c0c6219e25d
Victor López <2351772100101@ingenieria.usac.edu.gt>
2020-08-26 18:14:15 -0600

    actualizar alumno

--- database/CRUD_alumnos.sql
+++ database/CRUD_alumnos.sql
@@ -63,3 +63,50 @@ BEGIN
  END IF;
 END;
 $$
+
+# FUNCTION actualizar_alumno
+USE control_notas;
+DELIMITER $$
+CREATE FUNCTION actualizar_alumno ( 
+ p_alumno INT,
+ p_nombre VARCHAR(80),
+ p_apellido VARCHAR(80),
+ p_direccion VARCHAR(100),
+ p_telefono VARCHAR(12),
+ p_cui BIGINT,
+ p_encargado VARCHAR(80),
+ p_fecha_nacimiento DATE,
+ p_estado INTEGER
+)
+RETURNS JSON DETERMINISTIC
+BEGIN
+ DECLARE existe INT DEFAULT 0;
+
+ IF (SELECT nombre FROM alumno WHERE alumno = p_alumno) IS NULL THEN
+  SET existe = 0;
+ ELSE 
+  SET existe = 1;
+ END IF;
+
+ IF existe THEN
+  IF p_nombre IS NOT NULL AND p_apellido IS NOT NULL THEN
+   UPDATE alumno
+    SET 
+	 nombre = p_nombre,
+     apellido = p_apellido,
+	 direccion = p_direccion,
+     telefono = p_telefono,
+     cui = p_cui,
+     encargado = p_encargado,
+     fecha_nacimiento = p_fecha_nacimiento,
+	 estado = p_estado 
+      WHERE p_alumno = alumno;
+    RETURN '{"estado": 200, "mensaje": "Operación completada con exito."}';
+  ELSE 
+   RETURN '{"estado": 500, "mensaje": "El nombre y el apellido son valores obligatorio"}';
+  END IF;
+ ELSE
+  RETURN '{"estado": 500, "mensaje": "No existe la alumno que se desea actualizar."}';
+ END IF;
+END;
+$$
