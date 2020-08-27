b54d1ac1a2edb6658f911e7057de849e8f484070
Victor López <2351772100101@ingenieria.usac.edu.gt>
2020-08-27 05:02:57 -0600

    Update CRUD_materia.sql

--- database/CRUD_materia.sql
+++ database/CRUD_materia.sql
@@ -1,115 +1,90 @@
 # CRUD de la tabla MATERIA
 
-# FUNCTION insertar_materia
+# PROCEDURE insertar_materia
 USE control_notas;
 DELIMITER $$
-CREATE FUNCTION insertar_materia
-(
-    p_nombre     VARCHAR(70),
- p_contenido  VARCHAR(70)
-)
-RETURNS JSON DETERMINISTIC
+CREATE PROCEDURE insertar_materia ( p_nombre VARCHAR(70), p_contenido VARCHAR(70) )
 BEGIN
-  DECLARE existe INT DEFAULT 0;
-        
-        IF (SELECT nombre FROM materia 
-    WHERE LOWER(p_nombre) LIKE LOWER(nombre) 
-     AND LOWER(p_contenido) LIKE LOWER(contenido)
-      ) IS NULL THEN
-   SET existe = 0;
-  ELSE 
-   SET existe = 1;
-  END IF;
-        
-        IF existe THEN
-   RETURN '{"estado": 500, "mensaje": "Error: Ya existe la materia que se desea ingresar."}';
-        ELSE
-   IF p_nombre IS NOT NULL THEN
-      INSERT INTO  materia (nombre, contenido) VALUES ( p_nombre, p_contenido);
-    RETURN '{"estado": 200, "mensaje": "Operación completada con exito."}';
-   ELSE 
-    RETURN '{"estado": 500, "mensaje": "Error: El nombre es un valor obligatorio"}';
-   END IF;
-        END IF;
-END;
-$$
+ DECLARE existe INT DEFAULT 0;
 
-# FUNCTION obtener_materia
-USE control_notas
-DELIMITER $$
-CREATE FUNCTION obtener_materia
-(
-    p_materia INT
-)
-RETURNS JSON DETERMINISTIC
-BEGIN
-
- IF (SELECT materia FROM materia WHERE p_materia = materia) IS NOT NULL THEN
- RETURN (
-  SELECT 
-   JSON_OBJECT('nombre', nombre, 'contenido', contenido)
-   FROM materia
-    WHERE p_materia = materia
-            );
+ IF( SELECT nombre 
+      FROM materia 
+	   WHERE LOWER(p_nombre) LIKE LOWER(nombre) 
+	    AND LOWER(p_contenido) LIKE LOWER(contenido) 
+ ) IS NULL THEN
+  SET existe = 0;
  ELSE 
-  RETURN '{"error": 400, "mensaje": "No se puedo encontrar el registro en la tabla materia."}';
-    END IF;
-END;
-$$
+  SET existe = 1;
+ END IF;
 
-# FUNCTION actualizar_materia
-USE control_notas;
-DELIMITER $$
-CREATE FUNCTION actualizar_materia
-(
- p_materia  VARCHAR(70),
-    p_nombre     VARCHAR(70),
- p_contenido  VARCHAR(70)
-)
-RETURNS JSON DETERMINISTIC
-BEGIN
-  DECLARE existe INT DEFAULT 0;
-        
-        IF (SELECT nombre FROM materia 
-   WHERE materia = p_materia) IS NULL THEN
-   SET existe = 0;
-  ELSE 
-   SET existe = 1;
+ IF existe THEN
+    SELECT * FROM mensaje WHERE codigo = 501;
+ ELSE
+  IF p_nombre IS NOT NULL THEN
+   INSERT INTO  materia (nombre, contenido) VALUES ( p_nombre, p_contenido);
+   SELECT * FROM mensaje WHERE codigo = 200;
+  ELSE
+   SELECT * FROM mensaje WHERE codigo = 500;
   END IF;
-        
-        IF existe THEN
-   IF p_nombre IS NOT NULL THEN
-    UPDATE materia
-     SET 
-      nombre= p_nombre,
-      contenido = p_contenido
-     WHERE
-      p_materia = materia;
-    RETURN '{"estado": 200, "mensaje": "Operación completada con exito."}';
-   ELSE 
-    RETURN '{"estado": 500, "mensaje": "Error: El nombre es un valor obligatorio"}';
-   END IF;
-        ELSE
-   RETURN '{"estado": 500, "mensaje": "Error: No existe la materia que se desea actualizar."}';
-        END IF;
+ END IF;
+ 
 END;
 $$
 
-# FUNCTION eliminar_materia
+# PROCEDURE obtener_materia
 USE control_notas
 DELIMITER $$
-CREATE FUNCTION eliminar_materia
-(
-    p_materia INT
-)
-RETURNS JSON DETERMINISTIC
+CREATE PROCEDURE obtener_materia ( p_materia INT )
+BEGIN
+ IF (SELECT materia FROM materia WHERE p_materia = materia) IS NOT NULL THEN
+  SELECT * FROM materia WHERE p_materia = materia;
+ ELSE
+  SELECT * FROM mensaje WHERE codigo = 400;
+ END IF;
+END;
+$$
+
+# PROCEDURE actualizar_materia
+USE control_notas;
+DELIMITER $$
+CREATE PROCEDURE actualizar_materia ( p_materia VARCHAR(70), p_nombre VARCHAR(70), p_contenido  VARCHAR(70) )
+BEGIN
+ DECLARE existe INT DEFAULT 0;
+
+ IF (SELECT nombre FROM materia WHERE materia = p_materia) IS NULL THEN
+  SET existe = 0;
+ ELSE 
+  SET existe = 1;
+ END IF;
+
+ IF existe THEN
+  IF p_nombre IS NOT NULL THEN
+   UPDATE materia
+    SET 
+     nombre= p_nombre,
+     contenido = p_contenido
+      WHERE p_materia = materia;
+    SELECT * FROM mensaje WHERE codigo = 200;
+  ELSE 
+   SELECT * FROM mensaje WHERE codigo = 500;
+  END IF;
+ ELSE
+  SELECT * FROM mensaje WHERE codigo = 502;
+ END IF;
+END;
+$$
+
+# PROCEDURE eliminar_materia
+USE control_notas
+DELIMITER $$
+CREATE PROCEDURE eliminar_materia ( p_materia INT )
 BEGIN
  IF (SELECT materia FROM materia WHERE p_materia = materia) IS NOT NULL THEN
   DELETE FROM materia 
-   WHERE p_materia = materia;
-         RETURN '{"estado": 200, "mensaje": "Operación completada con exito."}';   
+  WHERE p_materia = materia;
+  SELECT * FROM mensaje WHERE codigo = 200;
  ELSE 
-  RETURN '{"error": 400, "mensaje": "No se puedo encontrar el registro en la tabla materia."}';
-    END IF;
+  SELECT * FROM mensaje WHERE codigo = 503;
+ END IF;
 END;
 $$
