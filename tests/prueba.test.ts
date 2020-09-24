10341779d93240b70baf60c8fca08b3c1da90320
Feliciano07 <fernandoxhajon122@gmail.com>
2020-09-24 12:08:18 -0600

    Aclaraciones sobre las configuracion realizadas

--- tests/prueba.test.ts
+++ tests/prueba.test.ts
@@ -4,17 +4,18 @@ import { expect } from "chai"
 // no use jasmine dado que no te mucha informacion sobre que pruebas estan bien 
 // solo da como puntitos verdes y solo te da mensajes cuando fallan
 // y asi no me gusta :p
-
+// esto solo es un archivo de como estaria la estructura basica para hacer su puebas
 
 describe('Hola1', ()=>{
+    // funciona
     it('Sumar', ()=>{
         expect(5,'funciona').to.equal(5);
     });
-
+    // falla
     it('Sumar2', ()=>{
         expect(5,'funciona').to.equal(6);
     });
-
+    // falla
     it('Sumar3', ()=>{
         expect(5,'funciona').to.equal(6);
     });
