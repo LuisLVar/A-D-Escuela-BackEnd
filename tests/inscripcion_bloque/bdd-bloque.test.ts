813e59d8dab8fa79c9d9962f47ec1fbcd2a647e5
Feliciano07 <fernandoxhajon122@gmail.com>
2020-09-25 14:48:58 -0600

    implementacion de codigo bdd Listar Bloques en .feature

---
+++ tests/inscripcion_bloque/bdd-bloque.test.ts
@@ -0,0 +1,27 @@
+import chai, {expect} from 'chai';
+import chaiHttp from 'chai-http';
+chai.use(chaiHttp);
+const url = 'http://3.20.3.133:3010';
+
+describe('Listar Bloques', ()=>{
+
+    let res: any ;
+
+    //@Given
+    it('Que se quiera ver los bloques', async() =>{
+        expect(url).to.equal(url);
+    });
+
+    //@When
+    it('Realizar una solicitud a los datos para extraer los bloques', async ()=>{
+        res = await chai.request(url)
+        .get('/api/bloques');
+        console.log(res);
+        expect(res).not.null;
+    });
+
+    //@Then
+    it('Obtener una lista de bloques', async ()=>{
+        expect(res.length).not.undefined;
+    })
+});
