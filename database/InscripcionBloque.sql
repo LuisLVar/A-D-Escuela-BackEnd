b4754c4839da7265171632a4047a49b62d8f2654
Luis Vargas <luisvarleon12@gmail.com>
2020-09-25 00:58:14 -0600

    Estructura de Procedimiento de incripcion bloque

---
+++ database/InscripcionBloque.sql
@@ -0,0 +1,11 @@
+-- Crear Inscripcion Bloque
+
+use control_notas;
+#PROCEDURE inscripcion_bloque;
+DROP PROCEDURE inscripcion_bloque;
+DELIMITER $$
+CREATE PROCEDURE inscripcion_bloque( p_seccion INT, p_alumno INT , p_bloque INT )
+BEGIN
+
+END
+$$
