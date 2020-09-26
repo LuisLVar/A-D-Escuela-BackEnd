db5799cd6ffc4a2d40ea4a8d89ba5123aa10b02a
Luis Vargas <luisvarleon12@gmail.com>
2020-09-26 03:08:39 -0600

    Estructura de listar grado

--- database/InscripcionBloque.sql
+++ database/InscripcionBloque.sql
@@ -9,3 +9,13 @@ BEGIN
 
 END
 $$
+
+use control_notas;
+#PROCEDURE listar_grado_bloque;
+DROP PROCEDURE listar_grado_bloque;
+DELIMITER $$
+CREATE PROCEDURE listar_grado_bloque( p_seccion INT, p_bloque INT )
+BEGIN
+
+END
+$$
